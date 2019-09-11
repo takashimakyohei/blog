0543af9114aac793e7a0dc9de242ebd843321234
高島恭平 <takashimakyouhei@takashimakyouhei-no-MacBook-Pro.local>
2019-09-11 13:08:01 +0900

    first-commit

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  validates :title, presence:true
+  validates :body, presence:true
+  mount_uploader :image, ImageUploader
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {
+   :registrations => 'users/registrations',
+   :sessions => 'users/sessions'
+  }
+  
+root "articles#index"
+
+  resources :articles;
+
+end
